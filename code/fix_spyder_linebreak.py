b906ac4b5ca369a2bf58bb86c1f8e522b7c5f15c
David O. Sigthorsson <sigthorsson@gmail.com>
2020-08-18 14:10:32 -0400

    fixed get_data2.py to allow redirects from JH Dataset

--- code/fix_spyder_linebreak.py
+++ code/fix_spyder_linebreak.py
@@ -1,5 +1,5 @@
 """
-Fix weird issue with linebreak characters from Spyder. 
+Fix weird issue with linebreak characters from Spyder.
 Linebreaks show up correctly in Spyder but not in Atom or GitHub.
 
 Reads the text in a given file and writes it back to another file.
@@ -16,12 +16,12 @@ root = tk.Tk()
 root.withdraw()
 
 # GUI request filenames (note, must use askopenfilenames, not askopenfile, because that also opens the files and I had trouble closing them without losing data)
-filenames = filedialog.askopenfilenames(initialdir='.',title = "Select file", filetypes=[["Python Files","*.py"]])
+filenames = filedialog.askopenfilenames(initialdir='.',title = "Select file", filetypes=[["Python Files","*.py"],["Script Files","*.sh"]])
 
 for f in filenames:
     fname = os.path.relpath(f)
     forg = shutil.copy(fname,fname+"_org")  # Copy the file for backup purposes
-    print(fname+" -> " +os.path.relpath(forg))    
+    print(fname+" -> " +os.path.relpath(forg))
     fr = open(forg,'r')     # Read the copied original, and write to a new file, this fixes the stupid end of line problem
     fw = open(fname,'w')
     fw.write(fr.read())
