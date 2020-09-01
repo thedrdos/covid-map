1cc8aa69a921f37e7ebb6d5fe7049562fe7017d0
David O. Sigthorsson <sigthorsson@gmail.com>
2020-09-01 13:37:02 -0400

    Moved old code to legacy folder

---
+++ code/legacy/run_full_map_site_update.sh
@@ -0,0 +1,41 @@
+# List of scripts to run
+script_list=(
+"get_data_for_maps.py"
+"process_data_for_maps.py"
+"make_external_datafiles.py"
+"map_4mon_standalone.py"
+"map_4mon_external_data.py"
+"make_website.py")
+
+# The following is the actual script execution
+SECONDS=0
+dur_arr=(0)
+i=0
+for s in ${script_list[@]};
+do
+  ((i=i+1))
+  echo "####################################"
+  echo "Running: " $s
+  echo "####################################"
+  python $s || exit $i
+
+  dur_arr+=($SECONDS)
+  echo "Completed in: $(($((${dur_arr[${#dur_arr[@]}-1]}-${dur_arr[${#dur_arr[@]}-2]})) / 60)) minutes and $(($((${dur_arr[${#dur_arr[@]}-1]}-${dur_arr[${#dur_arr[@]}-2]})) % 60)) seconds."
+done
+
+# Provide a summary of script runtimes
+echo "======================="
+echo "Summary:"
+echo "======================="
+i=0
+for s in ${script_list[@]};
+do
+  ((i=i+1))
+  echo $s
+  echo "    $(($((${dur_arr[$i]}-${dur_arr[$i-1]})) / 60)) minutes and $(($((${dur_arr[$i]}-${dur_arr[$i-1]})) % 60)) seconds."
+done
+echo "======================="
+echo "$(date)" # Show todays date
+echo "Script completed in: "
+duration=$SECONDS
+echo "    $(($duration / 60)) minutes and $(($duration % 60)) seconds."
